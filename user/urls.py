8b5b02c38c9fd2d491bc380e138683feb7bad7dc
Swap1019 <kianjafari660@gmail.com>
2023-09-30 21:44:00 +0330

    Created-user-login-logout

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Register
+from .views import Register,UserLogin,UserLogout
 
 urlpatterns = [
-    path('register/',Register.as_view(),name='register')
-] 
+    path('register/',Register.as_view(),name='register'),
+    path('login/',UserLogin.as_view(),name='login'),
+    path('logout/',UserLogout.as_view(),name='logout'),
+]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponseRedirect
 from .forms import SignupForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView,LogoutView
 
 class Register(CreateView):
     form_class = SignupForm
@@ -12,3 +13,17 @@ class Register(CreateView):
         user.is_active = False
         user.save()
         return HttpResponseRedirect("")
+
+
+class UserLogin(LoginView):
+    template_name = 'user/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('base:home')
+    
+class UserLogout(LogoutView):
+
+    def get_success_url(self):
+        return reverse_lazy('base:home')
