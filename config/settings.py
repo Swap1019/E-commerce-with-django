857102a0107ba9336d1448e3209b8cfff4df6258
Kian Jafari <kianjafari660@gmail.com>
2024-08-06 14:13:01 +0330

    requierments update
    requirements update

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-fxpqqk(iv-^f6sx(_4qy10-k!w3@6028(7!ae6gf#r#kn^d4s8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','192.168.1.87']
 
 
 # Application definition
